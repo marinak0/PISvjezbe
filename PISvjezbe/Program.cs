8f131e837fd0751e3e868a79249a5fb7d97e8824
marinak0 <73098191+marinak0@users.noreply.github.com>
2021-03-22 20:48:31 +0100

    Update Program.cs

--- PISvjezbe/Program.cs
+++ PISvjezbe/Program.cs
@@ -5,7 +5,7 @@ namespace PISvjezbe
     class Program
     {
         static void Main(string[] args)
-        {
+        { //Hej Michelle
             Console.WriteLine("Hello World!");
             Console.WriteLine("Sup");
         }
